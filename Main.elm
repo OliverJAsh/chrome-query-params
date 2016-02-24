4701b4341dc085eaaf829a58d38fa43b8d3241d8
Oliver Joseph Ash <oliverjash@gmail.com>
2016-02-24 21:25:38 +0000

    Tidy up

--- Main.elm
+++ Main.elm
@@ -25,18 +25,15 @@ type Action
 
 
 queryParams =
-  let
-    queryParamsStr =
-      "a=1&b=2&c=3&d=4&e=5&f=6&g=7&a=8"
-  in
-    split "&" queryParamsStr
-      |> List.map (split "=")
-      |> List.map
-          (\ls ->
-            ( List.head ls |> Maybe.withDefault ""
-            , List.Extra.getAt ls 1 |> Maybe.withDefault ""
-            )
+  "a=1&b=2&c=3&d=4&e=5&f=6&g=7&a=8"
+    |> split "&"
+    |> List.map (split "=")
+    |> List.map
+        (\ls ->
+          ( List.head ls |> Maybe.withDefault ""
+          , List.Extra.getAt ls 1 |> Maybe.withDefault ""
           )
+        )
 
 
 updateIn : List a -> Int -> a -> List a
